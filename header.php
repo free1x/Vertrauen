793bf804cba23f90c33d00277ce69fdebf5abf39
naco@nco.im <naco@nco.im>
2019-03-01 09:36:13 +0800

    Update

--- header.php
+++ header.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     <head>
-        <?php  $transfer = get_stylesheet_directory(); ?>
+        <?php  $transfer = get_stylesheet_directory_uri().'/static/'; ?>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
             <title><?php wp_title('-','-');?></title>
         <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
         <?php wp_head(); ?>
-        <link rel="stylesheet" href="<?php echo $transfer.'/static/css/style.css' ?>" type="text/css">
-        <script src=""></script>
+        <link rel="stylesheet" href="<?php echo $transfer.'/css/bootstrap.min.css' ?>" type="text/css">
+        <link rel="stylesheet" href="<?php echo $transfer.'/css/style.css' ?>" type="text/css">
+
+        <script src="<?php echo $transfer.'/js/jquery-3.3.1.min.js' ?>" type="text/javascript"></script>
+        <script src="<?php echo $transfer.'/js/bootstrap.min.js' ?>" type="text/javascript"></script>
     </head>
 
 <body <?php body_class(); ?>>

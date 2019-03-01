e016114f157d0eba10342cc280b3aa3fcff97088
naco@nco.im <naco@nco.im>
2019-03-01 09:28:27 +0800

    Update

--- header.php
+++ header.php
@@ -1,16 +1,14 @@
-<?php
-/**
- * @package WordPress
- * @subpackage Options Framework Theme
- */
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <title><?php wp_title('-','-');?></title>
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<?php wp_head(); ?>
-</head>
+    <head>
+        <?php  $transfer = get_stylesheet_directory(); ?>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+            <title><?php wp_title('-','-');?></title>
+        <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+        <?php wp_head(); ?>
+        <link rel="stylesheet" href="<?php echo $transfer.'/static/css/style.css' ?>" type="text/css">
+        <script src=""></script>
+    </head>
 
 <body <?php body_class(); ?>>
 

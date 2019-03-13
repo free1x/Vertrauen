8a5777e081bf27bb78227f7b7ec2e9e740e3ea98
naco@nco.im <naco@nco.im>
2019-03-13 11:17:29 +0800

    Update

--- functions.php
+++ functions.php
@@ -171,3 +171,25 @@ function Vertrauen_page( $range = 4 ) {
 
 
 add_filter( 'pre_option_link_manager_enabled', '__return_true' );
+
+
+add_action( 'widgets_init', 'my_unregister_widgets' );
+function my_unregister_widgets() {
+	unregister_widget( 'WP_Widget_Archives' );
+	unregister_widget( 'WP_Widget_Calendar' );
+	unregister_widget( 'WP_Widget_Categories' );
+	unregister_widget( 'WP_Widget_Links' );
+	unregister_widget( 'WP_Widget_Meta' );
+	unregister_widget( 'WP_Widget_Pages' );
+	unregister_widget( 'WP_Widget_Recent_Comments' );
+	unregister_widget( 'WP_Widget_Recent_Posts' );
+	unregister_widget( 'WP_Widget_RSS' );
+	unregister_widget( 'WP_Widget_Search' );
+	unregister_widget( 'WP_Widget_Tag_Cloud' );
+	unregister_widget( 'WP_Widget_Text' );
+	unregister_widget( 'WP_Nav_Menu_Widget' );
+
+
+
+
+}

7372a4278ae0124b7a02417d16fa8975b8bc1cdb
naco@nco.im <naco@nco.im>
2019-03-14 19:18:49 +0800

    Update

--- single.php
+++ single.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
-<div class="container single">
+<div class="container single p-0">
 	<div class="row single-content">
 		<div class="col-lg-8">
+			<?php if(function_exists('cmp_breadcrumbs')) cmp_breadcrumbs();?>
 
 		</div>
 		<div class="col-lg-4">

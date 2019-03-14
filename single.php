a5c5fbd8b7285e5438dbd10d5fc4e8c877d29629
naco@nco.im <naco@nco.im>
2019-03-14 20:24:46 +0800

    Update Single

--- single.php
+++ single.php
@@ -3,6 +3,23 @@
 	<div class="row single-content">
 		<div class="col-lg-8">
 			<?php if(function_exists('cmp_breadcrumbs')) cmp_breadcrumbs();?>
+			<div class="single-detail">
+				<div class="single_title">
+					<?php the_title(); ?>
+                </div>
+               <div class="single_info">
+                   <span> <i class="far fa-calendar-alt"></i> <?php the_time('Y年n月j日'); ?></span>
+                  <span> <i class="fas fa-comments"></i><?php comments_number(); ?></span>
+               </div>
+                <div class="single_post">
+	                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		                <?php echo the_content(); ?>
+	                <?php endwhile; endif; ?>
+                </div>
+            </div>
+
+
+
 
 		</div>
 		<div class="col-lg-4">
